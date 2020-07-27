da543c26630118b16903ba29e32843f2539b8246
Leticia Duarte Govoni <lgovoni@nexu.ca>
2020-07-27 09:46:08 -0300

    First commit

---
+++ DesafioAutomation/target/test-classes/Features/singIn.feature
@@ -0,0 +1,11 @@
+#language: pt
+
+Funcionalidade: Com um usuário
+	Desejo realizar um cadatro
+	Para que eu possa realizar login
+
+Cenário: Realizar um cadastro com sucesso
+Dado que o website foi acessado
+Quando clicar em singin
+E realizar um cadastro
+Então devo acessar minha conta
---
+++ DesafioWebSite/src/test/java/StepDefinitions/singInSteps.java
@@ -0,0 +1,65 @@
+package StepDefinitions;
+
+//import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.sun.corba.se.impl.oa.poa.ActiveObjectMap.Key;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+//import pages.authenticationPage;
+import pages.createAccountPage;
+import pages.loginPage;
+import pages.myAccountPage;
+
+public class singInSteps {
+	WebDriver driver = null;
+	loginPage login;
+	//authenticationPage authentication;
+	createAccountPage createAccount;
+	myAccountPage myAccount;
+
+	@Dado("^que o website foi acessado$")
+	public void que_o_website_foi_acessado() throws Throwable {
+		String projectPath = System.getProperty("user.dir");
+
+		System.setProperty("webdriver.chrome.driver",
+				projectPath + "\\src\\test\\resources\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("http://automationpractice.com/index.php");
+	}
+
+	@Quando("^clicar em singin$")
+	public void clicar_em_singin() throws Throwable {
+
+		login = new loginPage(driver);
+		login.enterLogin();
+	}
+
+	@Quando("^realizar um cadastro$")
+	public void realizar_um_cadastro() throws Throwable {
+
+		//driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);		
+		
+		createAccount = new createAccountPage(driver);
+		createAccount.authentication();
+		createAccount.personalInformation();
+	}
+
+	@Entao("^devo acessar minha conta$")
+	public void devo_acessar_minha_conta() throws Throwable {
+
+		//driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		myAccount = new myAccountPage(driver);
+		myAccount.creatAccout();
+
+	}
+
+};
